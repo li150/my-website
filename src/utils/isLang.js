4a88378426d0003f236fc6f8977c351277042971
pc-lyc <1449770614@qq.com>
2023-01-13 16:07:54 +0800

    pinia使用时需注意执行顺序

--- src/utils/isLang.js
+++ src/utils/isLang.js
@@ -1,10 +1,9 @@
 import { computed } from "vue";
 import langStore from "../store/lang";
 
-const langState = langStore();
-
 const isLang = () => {
-  const isLangEn = computed(() => langState.$state.lang !== "en");
+  const langState = langStore();
+  const isLangEn = computed(() => langState.$state.lang !== "zh");
   const isLangZh = computed(() => langState.$state.lang !== "en");
   const langFlag = computed(() =>
     langState.$state.lang !== "en" ? "必速派国际快递物流" : "Beespal Express",
